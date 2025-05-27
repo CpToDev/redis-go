43c3fa0122e2e19c1dcb13b4bd51460e96c06e2f
Saurav Shukla <sauravagtl@Richikas-MacBook-Pro.local>
2025-05-27 11:52:02 +0530

    Added get+set command with RWMutex

--- handler.go
+++ handler.go
@@ -8,6 +8,7 @@ var Handlers = map[string]func([]Value) *Value{
 	"PING":    ping,
 	"COMMAND": command,
 	"SET":     set,
+	"GET":     get,
 }
 
 func command(arr []Value) *Value {
@@ -17,7 +18,7 @@ func command(arr []Value) *Value {
 
 func ping(args []Value) *Value {
 	if len(args) == 0 {
-		return &Value{typ: "arr", arr: []Value{{typ: "bulk", bulk: "PONG"}}}
+		return &Value{typ: "string", str: "PONG"}
 	}
 	return &Value{typ: "arr", arr: args}
 
@@ -34,4 +35,18 @@ func set(args []Value) *Value {
 	SETs[args[0].bulk] = args[1].bulk
 	SETsMu.Unlock()
 	return &Value{typ: "string", str: "OK"}
+}
+
+func get(args []Value) *Value {
+	if len(args) != 1 {
+		return &Value{typ: "error", str: "Invalid no of arguments, expected 1 arguments"}
+	}
+	key := args[0].bulk
+	SETsMu.RLock()
+	defer SETsMu.RUnlock()
+	v, ok := SETs[key]
+	if !ok {
+		return &Value{typ: "null"}
+	}
+	return &Value{typ: "bulk", bulk: v}
 }
