3d8b33d3a8e0dfdc4941afd115854d6a06678d05
Saurav Shukla <sauravagtl@Richikas-MacBook-Pro.local>
2025-05-27 11:37:29 +0530

    Added set handler

--- handler.go
+++ handler.go
@@ -1,11 +1,17 @@
 package main
 
+import (
+	"sync"
+)
+
 var Handlers = map[string]func([]Value) *Value{
 	"PING":    ping,
 	"COMMAND": command,
+	"SET":     set,
 }
 
 func command(arr []Value) *Value {
+	// This is to generally handle the redis-cli initial connection
 	return &Value{typ: "bulk", bulk: "OK"}
 }
 
@@ -15,4 +21,17 @@ func ping(args []Value) *Value {
 	}
 	return &Value{typ: "arr", arr: args}
 
+}
+
+var SETs = map[string]string{}
+var SETsMu = sync.RWMutex{}
+
+func set(args []Value) *Value {
+	if len(args) != 2 {
+		return &Value{typ: "error", str: "Invalid no of arguments, expected 2 arguments"}
+	}
+	SETsMu.Lock()
+	SETs[args[0].bulk] = args[1].bulk
+	SETsMu.Unlock()
+	return &Value{typ: "string", str: "OK"}
 }
